bb9d6fe118588bbcbd0b436f6029e33de5214c2f
Kyle Fagnan <kfagnan19@gmail.com>
2021-10-27 09:14:37 -0600

    assertArraysEqual function

--- eqArrays.js
+++ eqArrays.js
@@ -10,16 +10,16 @@ const eqArrays = function(array1, array2) {
   let result = true;
   if (array1.length !== array2.length) {
     return false;
-  } 
-  for (let i = 0; i < array1.length; i++){
+  }
+  for (let i = 0; i < array1.length; i++) {
    
-    if (array1[i] !== array2[i]){
+    if (array1[i] !== array2[i]) {
       return false;
     }
     
   }
   return result;
-}
+};
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
 assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
 

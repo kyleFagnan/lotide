3734eb9033d4f1e065bd95a0a812b6a7c2d131c4
Kyle Fagnan <kfagnan19@gmail.com>
2021-11-01 19:23:27 -0600

    refactor middleTest with mocha and chai

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,2 +1,12 @@
-const assertArraysEqual = require('../assertArraysEqual');
-const eqArrays = require('../eqArrays');
+const middle = require('../middle');
+const assert = require('chai').assert;
+
+
+describe("#middle", () => {
+  it("returns [2] for [1, 2, 3]", () =>{
+    assert.deepEqual(middle([1, 2, 3]), [2]);
+  });
+  it("returns [2, 3] for [4, 2, 3, 4]", () =>{
+    assert.deepEqual(middle([4, 2, 3, 4]), [2, 3]);
+  });
+});
